417aaafd598854c81bb9bef91ff9d1c26ebbc320
CrazyProger1 <crazyproger1@gmail.com>
2024-01-04 18:31:54 +0200

    Add: object proxy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from src.core import run
+from src.utils import clsutils
+
+one = clsutils.ObjectProxy(1)
 
 if __name__ == '__main__':
     run()
---
+++ src/utils/clsutils/__init__.py
@@ -0,0 +1,8 @@
+from .generators import iter_instances, iter_subclasses
+from .proxy import ObjectProxy
+
+__all__ = [
+    'iter_instances',
+    'iter_subclasses',
+    'ObjectProxy'
+]
